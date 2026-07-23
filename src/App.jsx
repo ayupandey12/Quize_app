ea200b7dc2c6b0d3fe3f72901ebbbf72261c538d
ayupandey12 <ayushpandey9802@gmail.com>
2026-07-23 10:23:02 +0530

    added next button for doing on next question and auto 15 sec is also working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import a from "./assets/question.json"
 import { useEffect } from "react"
+import { Select } from "./components/Select"
 const questions=a.map((q)=>{return {id:q.id,question:q.question}})
 const options=a.map((q)=>{return {id:q.id,options:q.options}})
 const answers=a.map((q)=>{return {id:q.id,answer:q.correctAnswer}})
@@ -10,27 +11,29 @@ export const  App=()=>{
   const [curroption,setcurroption]=useState(options.filter((q)=>{return q.id===questionid}))
   const [curranswer,setcurranswer]=useState(answers.filter((q)=>{return q.id===questionid}))
   const [chooseanswer,setchooseanswer]=useState(null)
-  
+  const [settimer,settimert]=useState(null)
  useEffect(()=>{
+     if(questionid>questions.length) return
      setcurrquestion(questions.filter((q)=>{return q.id===questionid}))
      setcurroption(options.filter((q)=>{return q.id===questionid}))
      setcurranswer(answers.filter((q)=>{return q.id===questionid}))
      setchooseanswer(null)
      if(questionid==questions.length) return
-     setTimeout(() => {
+    const t=setTimeout(() => {
       setquestionid(questionid+1);
      }, 10000);
-  
+     settimert(t);
+     
  },[questionid])
  useEffect(()=>{
    console.log(chooseanswer)
  },[chooseanswer])
   
-  return <div>
+  return (<div>
     <div>{currquestion[0].question}</div>
-    <select defaultValue={"select the option below"} value={chooseanswer} name="quesop" id="quesop" onChange={(e)=>{setchooseanswer(e.target.value)}}>
-      <option disabled id="quesop" >{"select the option below"}</option>
-      {curroption[0].options.map((val)=>{return <option id="quesop">{val}</option>})}
-    </select>
-  </div>
+     <Select chooseanswer={chooseanswer} setchooseanswer={setchooseanswer} curroption={curroption}/>
+     <button onClick={()=>{setquestionid(questionid+1) 
+            clearTimeout(settimer)
+     }}>next</button>
+  </div>)
 }
