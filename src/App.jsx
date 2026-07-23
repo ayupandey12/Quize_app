8387f9119bd2a7079ed584ee2a63d0d443a324ac
ayupandey12 <ayushpandey9802@gmail.com>
2026-07-23 09:35:39 +0530

    updated the question showing in 2 sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react"
 import a from "./assets/question.json"
+import { useEffect } from "react"
+const questions=a.map((q)=>{return {id:q.id,question:q.question}})
+const options=a.map((q)=>{return {id:q.id,options:q.options}})
+const answers=a.map((q)=>{return {id:q.id,answer:q.correctAnswer}})
 export const  App=()=>{
+  const [questionid,setquestionid]=useState(1);
+  const [currquestion,setcurrquestion]=useState(questions.filter((q)=>{return q.id===questionid}))
+  const [curroption,setcurroption]=useState(options.filter((q)=>{return q.id===questionid}))
+  const [curranswer,setcurranswer]=useState(answers.filter((q)=>{return q.id===questionid}))
   
-  return <div>let's start</div>
+ useEffect(()=>{
+     setcurrquestion(questions.filter((q)=>{return q.id===questionid}))
+     setcurroption(options.filter((q)=>{return q.id===questionid}))
+     setcurranswer(answers.filter((q)=>{return q.id===questionid}))
+     if(questionid==questions.length) return
+     setTimeout(() => {
+      setquestionid(questionid+1);
+     }, 2000);
+  
+ },[questionid])
+ 
+  
+  return <div>
+    <div>{currquestion[0].question}</div>
+    <div>{curroption[0].options}</div>
+    <div>{curranswer[0].answer}</div>
+  </div>
 }
