d9c364ed2f26e0ab46ecfc8b908b965c302b6cc2
ayupandey12 <ayushpandey9802@gmail.com>
2026-07-24 09:36:11 +0530

    added score function

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { Select } from "./components/Select"
 const questions=a.map((q)=>{return {id:q.id,question:q.question}})
 const options=a.map((q)=>{return {id:q.id,options:q.options}})
 const answers=a.map((q)=>{return {id:q.id,answer:q.correctAnswer}})
+const size=questions.length;
+const ansrec=[]
+let iscalculated=false;
+ansrec.length=size+1
 export const  App=()=>{
   const [questionid,setquestionid]=useState(1);
   const [currquestion,setcurrquestion]=useState(questions.filter((q)=>{return q.id===questionid}))
@@ -12,11 +16,13 @@ export const  App=()=>{
   const [curranswer,setcurranswer]=useState(answers.filter((q)=>{return q.id===questionid}))
   const [chooseanswer,setchooseanswer]=useState(null)
   const [settimer,settimert]=useState(null)
+  const [score,setscore]=useState(0)
  useEffect(()=>{
      if(questionid>questions.length) return
      setcurrquestion(questions.filter((q)=>{return q.id===questionid}))
      setcurroption(options.filter((q)=>{return q.id===questionid}))
      setcurranswer(answers.filter((q)=>{return q.id===questionid}))
+     ansrec[questionid]=chooseanswer;
      setchooseanswer(null)
      if(questionid==questions.length) return
     const t=setTimeout(() => {
@@ -27,13 +33,20 @@ export const  App=()=>{
  },[questionid])
  useEffect(()=>{
    console.log(chooseanswer)
- },[chooseanswer])
-  
+ },[chooseanswer,iscalculated])
   return (<div>
     <div>{currquestion[0].question}</div>
      <Select chooseanswer={chooseanswer} setchooseanswer={setchooseanswer} curroption={curroption}/>
      <button onClick={()=>{setquestionid(questionid+1) 
             clearTimeout(settimer)
      }}>next</button>
+     {questionid>questions.length&&<button onClick={()=>{
+      for(let i=1;i<=size;i++)
+      {
+        if(ansrec[i]===answers[i-1].answer) setscore(score+1)
+      }
+      iscalculated=true
+     }}>submit</button>}
+     {iscalculated&&<div>your score is {score}</div>}
   </div>)
 }
